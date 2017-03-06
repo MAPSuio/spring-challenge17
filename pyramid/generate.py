146c34dc09eb129f37134760d8d85adade6e704e
Carl Martin Rosenberg <github@cmrosenberg.com>
2017-03-06 22:56:16 +0100

    Adding Pyramid

---
+++ pyramid/generate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def floor_cost(n):
+    return 2 + (n-1)*2
+
+def total_cost(n):
+    return (n  * (floor_cost(1) + floor_cost(n))) // 2
+
+
+
+#Chosen to give a number > 10 ** 12
+print(total_cost(50004999))
