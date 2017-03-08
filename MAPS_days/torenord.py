0bd94b18c3bd445b93d10b9164f46721a2c110ca
Tore Norderud <tore.norderud@gmail.com>
2017-03-08 18:40:01 +0100

    maps_days: Fix

--- MAPS_days/torenord.py
+++ MAPS_days/torenord.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 def maps_days_in_2000s():
     d = datetime(2000, 1, 1)
 
-    while d < datetime.now():
+    while d <= datetime.now():
         if sum(int(i) for i in str(d.day) + str(d.month)) == sum(int(i) for i in str(d.year)):
             yield 1
 
